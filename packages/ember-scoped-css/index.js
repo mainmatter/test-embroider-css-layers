f3f18f825c5e9146f5be1ddb3b87261de2ff15b9
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2023-02-06 11:41:18 +0100

    rollup-css-colocation divided into two plugins

--- packages/ember-scoped-css/index.js
+++ packages/ember-scoped-css/index.js
@@ -1,13 +1,16 @@
 const scopedHbsLoader = require('./src/scoped-hbs-loader');
 const scopedCssLoader = require('./src/scoped-css-loader');
 const scopedJsLoader = require('./src/scoped-js-loader');
-const rollupCssColocation = require('./src/rollup-css-colocation');
+
+const rollupHbsPlugin = require('./src/rollup-hbs-plugin');
 const rollupCssPlugin = require('./src/rollup-css-plugin');
+const rollupJsPlugin = require('./src/rollup-js-plugin');
 
 module.exports = {
   scopedHbsLoader,
   scopedCssLoader,
   scopedJsLoader,
-  rollupCssColocation,
+  rollupHbsPlugin,
   rollupCssPlugin,
+  rollupJsPlugin,
 };
---
+++ packages/ember-scoped-css/src/rollup-js-plugin.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { readFileSync, existsSync } = require('fs');
+const getPostfix = require('./getPostfix');
+const rewriteCss = require('./rewriteCss');
+const replaceHbsInJs = require('./replaceHbsInJs');
+const getClassesTagsFromCss = require('./getClassesTagsFromCss');
+const rewriteHbs = require('./rewriteHbs');
+
+module.exports = function rollupCssColocation(options = {}) {
+  return {
+    name: 'rollup-js-plugin',
+
+    transform(code, id) {
+      if (id.endsWith('js')) {
+        const cssPath = id.replace('.js', '.css');
+
+        // check if the css file exists
+        const cssExists = existsSync(cssPath);
+        if (cssExists) {
+          const cssFileName = path.basename(cssPath);
+
+          // if colocated css file exists, import it
+          const transformed = `import './${cssFileName}';\n${code}`;
+          return transformed;
+        }
+      }
+    },
+  };
+};
